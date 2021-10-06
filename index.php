2d948b8004ca46e6800acbed55a76e1539a0bf40
CoDe-Warr1ors <92028869+CoDe-Warr1ors@users.noreply.github.com>
2021-10-06 16:58:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Admin Login</title>
+<link rel="stylesheet" type="text/css" href="css/s1.css">
+
+</head>
+<body>
+<div id="full">
+<div id="inner_full">
+<div id = "header"><H1>Blood Bank Management System</H1></div>
+<div id ="body"><br><br><br><br><br><br>
+		<table align = "center">
+			<tr>
+		<td width = "200px" height = "70px"><b><h3>Enter Username</h3></b></td>
+		<td width = "100px" height = "70px"><input type = "text" name = "UN" placeholder = "Enter Username" style=" width: 180px;height: 40px; border-radius:10px;"></td>
+		</tr>
+		<tr>
+			<td width = "200px" height= "70px"><b><h3>Enter Password</h3></b></td>
+			<td width = "200px" height = "70px"><input type = "text" name = "PSWD" placeholder = "Enter Password" style=" width: 180px;height: 40px; border-radius:10px;"></td>
+		</tr>
+		<tr>
+			<td><input type = "submit" name = "SUB" value= "login" style=" width: 70px;height: 40px; border-radius:3px;"></td>
+		</tr>
+		</table>
+		</div>
+<div id = "footer"><H3>Created by GOLD team members by love</H3></div>
+</div>
+</div>
+
+</body>
+</html>
